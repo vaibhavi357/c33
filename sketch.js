01ac439cbb796fec6cb456763e29eb5383509107
vaibhavi357 <74657006+vaibhavi357@users.noreply.github.com>
2021-02-17 17:51:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    pig3.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    //if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    //}
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+    gameState = "launched";
+}
+
+function keyPressed(){
+    //check the speed to avoid bird resetting back while the bird is still moving
+    if(keyCode === 32 && bird.body.speed < 1){
+        // empty the trajectory array when the space bar is pressed so that multiple trajectories are not created on the way back
+        bird.trajectory = [];
+        //reset the position of the bird near the slingshot to avoid the rapid swinging of the rubber band when attached from far off
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
+       slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
+
+// tips and tricks for debugging
+// 1. do not panic
+// 2. go to the console, check for errors and clues like in which file, line no. and what is the problem
+// 3. if no errors in the console then use anyone of the following
+// 3.1. comment sections of your code to narrow down the search for errors
+// 3.2. print values of critical variables 
+// 3.3. print messages in if and else block to check which block has executed
+// 3.4. combination of 3.1, 3.2, 3.3 can be used
+
+// most common errors
+// 1. typos = misspelled variables, functions
+// 2. incorrect use of functions
+// 3. usingvariables out of their scope
+
+// ways to prevent errors
+// 1. format or indent correctly
+// 2. give proper names of variables, functions
+// 3. add proper comments
+// 4. use functions (modular programming)
